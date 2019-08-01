3b25da7e5d77118b599dcc26f9fccb1fb8522fcd
Md Erum <mderum3@gmail.com>
2019-08-01 22:34:24 +0530

    Create whats Your Name.py

---
+++ whats Your Name.py
@@ -0,0 +1,27 @@
+# Sample Input 0
+
+# Ross
+# Taylor
+# Sample Output 0
+
+# Hello Ross Taylor! You just delved into python.
+
+
+
+
+
+a=input()
+b=input()
+
+
+
+def print_full_name(a, b):
+    c='! You just delved into python.'
+
+    
+    print("Hello ",a ,b+c)
+    
+
+if l<=10:
+    
+    print_full_name(a,b,c)    
